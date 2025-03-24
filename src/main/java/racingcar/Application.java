ba5173998d02620976705de8e0f9facb696f819b
Noh Hee Jun <ted4519@naver.com>
2025-03-24 20:11:59 +0900

    전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다. 주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,9 +6,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String[] carNames = getCarNames();
-
+        int[] distance = new int [carNames.length];
         // 시도할 회수를 입력받음
         int attempts = getNumberOfAttempts();
+
+        racing(distance,attempts,carNames);
 
     }
 
@@ -21,7 +23,7 @@ public class Application {
         return names.split(",");
     }
 
-    // 시도할 회수를 입력받아 반환하는 메서드
+    // 시도할 횟수를 입력받아 반환하는 메서드
     public static int getNumberOfAttempts() {
         System.out.print("시도할 회수는 몇 회인가요? ");
         String num = Console.readLine();
@@ -29,4 +31,43 @@ public class Application {
         // 입력받은 문자열을 정수로 변환하여 반환
         return Integer.parseInt(num);
     }
+
+    public static void racing(int[] distance,int num, String[] carNames) {
+        for (int i = 0; i < carNames.length; i++) {
+            distance[i] = 0;
+        }
+
+        System.out.println("\n실행 결과\n");
+
+        for (int i = 0; i < num; i++) {
+            gogo(distance);
+            printDist(carNames, distance);
+            System.out.println();
+        }
+    }
+
+    public static void gogo(int[] distance) {
+        for (int i = 0; i < distance.length; i++) {
+            distance[i] = distance[i] + move();
+        }
+    }
+
+    public static int move() {
+        int m=0;
+        if(Randoms.pickNumberInRange(0,9) >= 4) m=1;
+        return m;
+    }
+
+    public static void printDist(String[] carNames, int[] distance) {
+        for (int i = 0; i < carNames.length; i++) {
+            System.out.print(carNames[i] + ":");
+            for (int j = 0; j < distance[i]; j++) {
+                System.out.print("-");
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+
 }
