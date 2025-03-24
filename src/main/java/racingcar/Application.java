ef3a1c4b399f3517f0b3c5eabe2305ffc92a6dc9
Noh Hee Jun <ted4519@naver.com>
2025-03-24 21:00:25 +0900

    자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다. 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -4,13 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private static final int MOVING_FORWARD = 4;
     public static void main(String[] args) {
         String[] carNames = getCarNames();
+        exceptname(carNames);
         int[] distance = new int [carNames.length];
         // 시도할 회수를 입력받음
         int attempts = getNumberOfAttempts();
-
         racing(distance,attempts,carNames);
+
+        printWinner(carNames,distance);
 
     }
 
@@ -27,10 +30,30 @@ public class Application {
     public static int getNumberOfAttempts() {
         System.out.print("시도할 회수는 몇 회인가요? ");
         String num = Console.readLine();
-
+        exceptnum(num);
         // 입력받은 문자열을 정수로 변환하여 반환
         return Integer.parseInt(num);
     }
+
+    public static void exceptname(String[] carNames) {
+        int max=0;
+        int min=2147483647;
+        for (int i=0;i<carNames.length;i++) {
+            if(carNames[i].length()>max) max = carNames[i].length();
+            if(carNames[i].length()<min) min = carNames[i].length();
+        }
+        if(max > 5 || min < 1 || min > max){
+            throw new IllegalArgumentException("각 자동차 이름의 길이는 1~5 범위 내로 입력하세요.");
+        }
+    }
+
+
+    public static void exceptnum(String attempts) {
+        if(!Character.isDigit(attempts.charAt(0))){
+            throw new IllegalArgumentException("올바른 수를 입력하세요.");
+        }
+    }
+
 
     public static void racing(int[] distance,int num, String[] carNames) {
         for (int i = 0; i < carNames.length; i++) {
@@ -54,13 +77,13 @@ public class Application {
 
     public static int move() {
         int m=0;
-        if(Randoms.pickNumberInRange(0,9) >= 4) m=1;
+        if(Randoms.pickNumberInRange(0,9) >= MOVING_FORWARD) m=1;
         return m;
     }
 
     public static void printDist(String[] carNames, int[] distance) {
         for (int i = 0; i < carNames.length; i++) {
-            System.out.print(carNames[i] + ":");
+            System.out.print(carNames[i] + " : ");
             for (int j = 0; j < distance[i]; j++) {
                 System.out.print("-");
             }
@@ -68,6 +91,24 @@ public class Application {
         }
     }
 
+    public static void printWinner(String[] carNames, int[] distance) {
+        boolean result = false;
+        int max = maxdis(distance);
+        System.out.print("최종 우승자 : ");
+        for (int i = 0; i < distance.length; i++) {
+            if(result && distance[i] == max) System.out.print(", ");
+            if(distance[i] == max) {
+                System.out.print(carNames[i]);
+                result = true;
+            }
+        }
+    }
 
-
+    public static int maxdis(int[] distance) {
+        int max = 0;
+        for (int i = 0; i < distance.length; i++) {
+            if(distance[i] > max) max = distance[i];
+        }
+        return max;
+    }
 }
