ab5750eee4bac9f7a579ce997176980c8db4a5e6
Noh Hee Jun <ted4519@naver.com>
2025-03-24 19:04:06 +0900

    각 자동차에 이름을 부여할 수 있다. 자동차 이름은 쉼표(,)를 기준으로 구분, 이름은 5자 이하만 가능 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNames = getCarNames();
+
+        // 시도할 회수를 입력받음
+        int attempts = getNumberOfAttempts();
+
+    }
+
+    // 경주할 자동차 이름을 입력받고, 쉼표로 구분된 이름들을 배열로 반환하는 메서드
+    public static String[] getCarNames() {
+        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
+        String names = Console.readLine();
+
+        // 쉼표로 나누어 이름을 배열로 반환
+        return names.split(",");
+    }
+
+    // 시도할 회수를 입력받아 반환하는 메서드
+    public static int getNumberOfAttempts() {
+        System.out.print("시도할 회수는 몇 회인가요? ");
+        String num = Console.readLine();
+
+        // 입력받은 문자열을 정수로 변환하여 반환
+        return Integer.parseInt(num);
     }
 }
