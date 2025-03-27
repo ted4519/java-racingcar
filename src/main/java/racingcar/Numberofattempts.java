ae9b7bd429f2fc1f3acba195723e5e2076831348
Noh Hee Jun <ted4519@naver.com>
2025-03-27 12:42:27 +0900

    시도 횟수를 입력받고 입력받은 값이 올바른지 확인하는 클래스

---
+++ src/main/java/racingcar/Numberofattempts.java
@@ -0,0 +1,21 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Numberofattempts {
+    public Numberofattempts() {
+    }
+
+    public int getnumberofattempts() {
+        System.out.print("시도할 회수는 몇 회인가요? ");
+        String attempts = Console.readLine();
+        this.exceptnum(attempts);
+        return Integer.parseInt(attempts);
+    }
+
+    private void exceptnum(String attempts) {
+        if (!Character.isDigit(attempts.charAt(0))) {
+            throw new IllegalArgumentException("올바른 수를 입력하세요.");
+        }
+    }
+}
