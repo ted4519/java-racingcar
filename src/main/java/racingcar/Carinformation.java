e42817b29521d0101c950031baa4dfcc95d3b506
Noh Hee Jun <ted4519@naver.com>
2025-03-31 14:25:06 +0900

    자동차의 이름들을 입력 받고 입력 받은 이름이 조건에 부합하는지 확인하는 클래스

--- src/main/java/racingcar/Carinformation.java
+++ src/main/java/racingcar/Carinformation.java
@@ -10,26 +10,15 @@ public class Carinformation {
         System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분): ");
         String carnames = Console.readLine();
         String[] eachcarnames = carnames.split(",");
-        this.exceptname(eachcarnames);
+        this.checkcarname(eachcarnames);
         return eachcarnames;
     }
 
-    private void exceptname(String[] carNames) {
-        int max = 0;
-        int min = Integer.MAX_VALUE;
-
+    private void checkcarname(String[] carNames) {
         for(int i = 0; i < carNames.length; ++i) {
-            if (carNames[i].length() > max) {
-                max = carNames[i].length();
+            if (carNames[i].length() > 5 ||carNames[i].length() < 1) {
+                throw new IllegalArgumentException("각 자동차 이름의 길이는 1~5 범위 내로 입력하세요.");
             }
-
-            if (carNames[i].length() < min) {
-                min = carNames[i].length();
-            }
-        }
-
-        if (max > 5 || min < 1 || min > max) {
-            throw new IllegalArgumentException("각 자동차 이름의 길이는 1~5 범위 내로 입력하세요.");
         }
     }
 }
